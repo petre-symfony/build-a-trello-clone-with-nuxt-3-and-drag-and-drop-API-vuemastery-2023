d524f52011a55a5a5149bab79409947917512765
petrero <petrero@gmail.com>
2024-01-16 18:04:29 +0200

    3.2 Layoutt the index page

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,19 @@
 </script>
 
 <template>
-  <main>
-    <h1>Build a Trello Clone Home Page</h1>
-    <pre>{{ boardStore.board }}</pre>
-  </main>
+  <div class="boarrd-wrapper">
+    <main class="board">
+      <div v-for="column in boardStore.board.columns" :key="column.name" class="column">
+        <h2>{{ column.name }}</h2>
+        <ul>
+          <li v-for="task in column.tasks" :key="task.id">
+            <strong>{{ task.name }}</strong>
+            <p>{{ task.description }}</p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  </div>
 </template>
 
 <style scoped>

fb5b0d7ce406a3fc2b91306badd4c3c8016ec880
petrero <petrero@gmail.com>
2024-01-16 18:32:50 +0200

    3.3 This is where we're gonna introduce and talk about some of the components that we get with the next ui. And so inside of here you'll see here that there are a couple of layout components that we're able to use.
     So for example, you have a container here that allows you to actually center and constrain. You have an actual card here that has like a header, a footer, a, it's, it's great. And so what we're gonna do here is we're actually gonna use this, basically start laying that out.
    
     So for example, for the column, rather than just have like an A list element that basically wraps around all of this. And so let's go ahead for this. Let's go and wrap it around with the U card component. Do do, do do, do save. Okay, great. We save that and we go back now to our logo host. You'll see, there you go, this looks much better now.
    
     So that's cool, we have that. And then I think for the column though, this probably makes sense to be an actual U container because this is no longer just like a div. This is actually a thing that we want to contain the stuff in.
    
     So U container save and there we go. Now things are looking a lot cleaner. Okay, this is good. So we have basically starting to utilize some of the layout components and then we will go more into that.

--- pages/index.vue
+++ pages/index.vue
@@ -7,15 +7,17 @@
 <template>
   <div class="boarrd-wrapper">
     <main class="board">
-      <div v-for="column in boardStore.board.columns" :key="column.name" class="column">
+      <UContainer v-for="column in boardStore.board.columns" :key="column.name" class="column">
         <h2>{{ column.name }}</h2>
         <ul>
           <li v-for="task in column.tasks" :key="task.id">
-            <strong>{{ task.name }}</strong>
-            <p>{{ task.description }}</p>
+            <UCard>
+              <strong>{{ task.name }}</strong>
+              <p>{{ task.description }}</p>
+            </UCard>
           </li>
         </ul>
-      </div>
+      </UContainer>
     </main>
   </div>
 </template>

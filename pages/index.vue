c73b6b298be8a1471893e6099189f948a97b90c1
petrero <petrero@gmail.com>
2024-01-17 11:19:51 +0200

    4.7 Alright, so the next thing we gotta do though is we got the C, we got the create we can read, uh, we still haven't got update and delete. So how do we do that? Well the way we're gonna do this is we're gonna need to update the UI a little bit, right? Well essentially what we need is a way to toggle this piece on the name incident input and then be able to basically update it. So the way I think I would approach this here is let's close that out and hide that just to make things a little bit easier. Okay? Inside of here we have our column name.
     I'm gonna go ahead and wrap this with the div and this diviv here. We're gonna go ahead and I have this helper right here called column header and basically just provides a little bit of structure for the stuff up here. So I'm just call it column header, just so for a little spacing and alignment. And then we're basically gonna have two things, right? We're gonna have the input and basically this is gonna be the model column name. Alright? And so we save that and we looked, there we go.
    
     We have in progress, in progress. If we update this, it actually does update the name as we see, okay?

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,10 @@
   <div class="boarrd-wrapper">
     <main class="board">
       <UContainer v-for="column in boardStore.board.columns" :key="column.name" class="column">
-        <h2 class="mb-4">{{ column.name }}</h2>
+        <div class="column-header">
+          <h2 class="mb-4">{{ column.name }}</h2>
+          <UInput type="text" v-model="column.name" />
+        </div>
         <ul>
           <li v-for="task in column.tasks" :key="task.id">
             <UCard class="mb-4">

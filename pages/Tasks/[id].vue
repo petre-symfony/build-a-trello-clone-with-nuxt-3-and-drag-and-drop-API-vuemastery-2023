75c963f25d5368e2465b58fc93454a8f5e475c55
petrero <petrero@gmail.com>
2024-01-17 16:20:57 +0200

    5.4 So okay, we have the get tasks now and so let's actually see if this works as intended. So we go inside of here, we need to actually import now the board store.
     So you use board store. Perfect. Um, I don't love the double dot for navigation because essentially what that does is it constrains you that in case this file ever moves, it'll break. So the Tilda inside of Nest three allows you to basically say this is the re directory no matter where you move, which is very convenient. So then we have that, then we can say con's, board store equals used board store.
    
     And then what we can do now is let's say Con's task is a computer property and basically we're gonna return board store dot get task and then we're gonna need the route do Peram. Id just like that and if this works as intended, we should get the task inside of here. Okay, fingers crossed, refresh. Alright, there we go. We have the id, we have the name, we have the description.
    
     So this is great. We now have the ability to read the specific task and get it from it.

--- pages/Tasks/[id].vue
+++ pages/Tasks/[id].vue
@@ -1,11 +1,16 @@
 <script setup>
+  import { useBoardStore } from "~/store/boardStore";
+
   const route = useRoute();
+  const boardStore = useBoardStore();
+
+  const task = computed(() => boardStore.getTask(route.params.id))
 </script>
 
 <template>
   <div class="task-wrapper">
     <div class="task-view">
-      <h1>Page: {{ route.params.id }}</h1>
+      <h1>Page: {{ task }}</h1>
     </div>
   </div>
 </template>

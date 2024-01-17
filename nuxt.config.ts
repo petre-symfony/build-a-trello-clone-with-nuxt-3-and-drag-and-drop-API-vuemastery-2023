bc9c0fc0ccb43edb30d931a559b6e4cf02a6a293
petrero <petrero@gmail.com>
2024-01-17 11:10:00 +0200

    4.6  So now I'm gonna go ahead and refresh the page and wait a second. It's go on. No need to Fred. Um, in fact, I came across this myself and at first you're wondering, you're like, what did I do wrong? I didn't, I set up like use storage. Is it supposed to be local? Like I saw the data, right?
     We saw the data change and like, so you come back here and you looking at application and we look in here, the board and you're just like, oh, like what? Like I definitely saw that change. I was not, I did not hallucinate that there is a recording to show that that was there, right? I could, but honestly that's how I felt when I, I was doing it.
    
     Uh, try to debug this. So in case you ever feel that way, just know I've been there too. Uh, common thing that most people forget about when they're using NEX three is that it is SSR first. In other words, server side rendering. Without getting into the details of that, essentially it's doing its own thing to manage state and that kinda stuff, which means that it's actually not talking to the browser first it's kind of doing its own thing. So for the purposes of this app, if your intention is to use local storage, and to be honest, SSR requires its own deployment strategy and whatnot, what we have to do is basically come inside of the next config and disable SSR because otherwise they're gonna go through and replace stuff. And once again, deployment SSR, this is a very complex topic. A lot of people think they need it and it just, it just depends on your scenario and how it works. All I can tell you my, my short advice regarding SS R is learn the different rendering strategies that make the most sense and figure out what works for the problem you're trying to solve rather than just don't take the general advice of like, oh everyone needs SSR for the fastest app. SSR doesn't always mean fastest, um, for a multitude of different reasons. Uh, there are problems in engineering complex and just as a reminder as we progress for all of us in our engineering journeys is that understanding trade-offs is what makes us good engineers thinking that any one thing is the pure best way of doing things often leads us to common mistakes and pitfalls that we would've otherwise predicted had we actually thought things through from a more objective perspective. Okay, that said, off my soapbox, let's refresh that now. Okay, if we go into blocked and we refresh, there you go.
    
     Now our local storage is working as expected, which is fantastic. Cool.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   css: ['~/assets/css/vello.css'],
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@pinia/nuxt', '@vueuse/nuxt'],

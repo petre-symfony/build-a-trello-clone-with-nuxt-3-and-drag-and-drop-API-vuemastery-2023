24b4b6f6b963dabd8fc33fe66139ae67c979a550
petrero <petrero@gmail.com>
2024-01-17 12:20:47 +0200

    4.13 Now the other thing though is that you'll notice that when we click edit, it's editing everything. Everything is toggled to edit.
     And that doesn't make a lot of sense, right? Because after all you're really trying to edit the one column.
    
     And so this is part of the reason why we're probably seeing this weird like typing issue where you'll notice that it goes out of focus because it's just having trouble keeping track. So it's this case where I think it's actually a good time to go ahead and let's just quickly refactor this just to have more component level state and not have to deal with it on a bigger level.
    
     So to do this real quick, we're gonna go ahead and create a components folder inside of our next app.
    
     And then we'll call this board column BoardColumn vue. Alright, so script set up here and then we'll have the template.
    
     And I'm gonna go ahead and split this up onto the right hand side so that it's easier to follow.
    
     But essentially we take a look at here we basically have this U container, which is the entire column here.
    
     And so if we go ahead and copy this and drop this in here, this contains a decent chunk of stuff.
    
     Well first of all, we don't need the loop anymore. We don't need the key. That should be good. But then we have a couple things going on because if we now delete this and replace this with our new component, this will be basically board column. It's gonna be iterated throughn and we can basically delete all this other stuff inside of here because we're just looping through the board column and rendering it.
    
     And the only other thing though is that we need to actually pass it the column, right? 'cause we're looping through it and we wanna pass it in.
    
     So column is column and it looks like we need to column index as well. So I'm gonna go ahead and pass in column index and then pass that through, just like that. And so now inside of our component, I'm gonna go ahead and save this and close.
    
     Let's go ahead and wrap this up. So we have a defined prop. And this is a macro in case you're new to this inside of script setup that makes it easier for you to define props within the composition. API context, we had the actual column itself type objects required true. And then we also had the column index, which is a number also required. True. Okay, great.

--- pages/index.vue
+++ pages/index.vue
@@ -19,33 +19,12 @@
 <template>
   <div class="boarrd-wrapper">
     <main class="board">
-      <UContainer v-for="(column, columnIndex) in boardStore.board.columns" :key="column.name" class="column">
-        <div class="column-header mb-4">
-          <div>
-            <UInput v-if="editNameState" type="text" v-model="column.name" />
-            <h2 v-else>{{ column.name }}</h2>
-          </div>
-          <div>
-            <UButton
-                icon="i-heroicons-pencil-square"
-                class="mr-2"
-                @click="editNameState = !editNameState"
-            />
-            <UButton
-                icon="i-heroicons-trash" color="red"
-                @click="deleteColumn(columnIndex)"
-            />
-          </div>
-        </div>
-        <ul>
-          <li v-for="task in column.tasks" :key="task.id">
-            <UCard class="mb-4">
-              <strong>{{ task.name }}</strong>
-              <p>{{ task.description }}</p>
-            </UCard>
-          </li>
-        </ul>
-      </UContainer>
+      <BoardColumn
+          v-for="(column, columnIndex) in boardStore.board.columns"
+          :key="column.name"
+          :column="column"
+          :columnIndex="columnIndex"
+      />
       <UContainer class="column">
         <UInput
             v-model="newColumnName"

dd743dcff6b644ccfe7c7d88deefe59cbfe7f848
petrero <petrero@gmail.com>
2024-01-18 18:54:31 +0200

    6.11 And so what we gotta do here is we gotta take it one step further inside of our delete task.
     Not only do we need to delete tasks, but we need to actually run a check here in our ui, basically say, hey look, does said tasks exist? Because if we can't find the task inside of here, the rest of the UI is ultimately moot. And so there's certainly like a VLS for like, you know, no task found.
    
     So we can do that real quick. The task view is fine here. So what I would do in this particular case is I would just wrap that and then we can go ahead and say basically for this section and then versus this section. And so we basically saying V if if a task exists,
    
     we're gonna display all that ui. Otherwise we'll say in Vls P, task not found. Okay, great. So that covers that scenario.
    
     But we need to do one more thing whenever we delete a task is because ultimately when we delete it, we really don't wanna just see an empty state. There's no real point to that. It is. So we need to just make sure router dot push, go back to the homepage. And since we haven't defined the router cons, router equals news router safe. Alright, refresh Task nine.

--- pages/index/tasks/[id].vue
+++ pages/index/tasks/[id].vue
@@ -2,29 +2,36 @@
   import { useBoardStore } from "~/store/boardStore";
 
   const route = useRoute();
+  const router = useRouter();
   const boardStore = useBoardStore();
 
   const task = computed(() => boardStore.getTask(route.params.id))
 
   function deleteTask() {
     boardStore.deleteTask(route.params.id);
+    router.push('/');
   }
 </script>
 
 <template>
   <div class="task-wrapper">
     <div class="task-view">
-      <UFormGroup label="name" class="mb-4 w-full">
-        <UInput type="text" v-model="task.name"/>
-      </UFormGroup>
-      <UFormGroup label="description" class="mb-4 w-full">
-        <UTextarea v-model="task.description" />
-      </UFormGroup>
-      <UButton
-          icon="i-heroicons-trash"
-          color="red"
-          @click="deleteTask"
-      >Delete task</UButton>
+      <section v-if="task">
+        <UFormGroup label="name" class="mb-4 w-full">
+          <UInput type="text" v-model="task.name"/>
+        </UFormGroup>
+        <UFormGroup label="description" class="mb-4 w-full">
+          <UTextarea v-model="task.description" />
+        </UFormGroup>
+        <UButton
+            icon="i-heroicons-trash"
+            color="red"
+            @click="deleteTask"
+        >Delete task</UButton>
+      </section>
+      <section v-else>
+        <p>Task not found</p>
+      </section>
     </div>
   </div>
 </template>

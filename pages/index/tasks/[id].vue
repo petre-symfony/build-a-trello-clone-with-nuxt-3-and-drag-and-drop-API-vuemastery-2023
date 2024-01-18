f89e7293b9831682cb0789326501de2ffc7726a4
petrero <petrero@gmail.com>
2024-01-18 19:01:26 +0200

    6.12 Oh, it is not looking as expected. What's going on here? Oh, I know I messed up the styling by wrapping it in this 'cause it should be done directly in the view. So again, for the sake of just not focusing on the CSS right now, we're gonna actually not group it that way just for the sake of making things a little bit easier like that.
     And then we'll just make these ths to make it consistent. That should fix it. Okay, great. All right, so we delete it now you see we are able to delete the task. Hurrah. This is great. Okay, so we've walked through a couple of different scenarios now, walked through different patterns as far as how you might be able to perform the task, but now we basically have the full suite of functionality inside of our Trello board as far as being able to create read, update, and to delete all of our columns and tasks.
    
     And now that we've learned how to do that with all of our new tooling and within next three, it's time for us to move on to the next lesson where we dive into the nitty gritty of dragging and dropping.

--- pages/index/tasks/[id].vue
+++ pages/index/tasks/[id].vue
@@ -15,23 +15,22 @@
 
 <template>
   <div class="task-wrapper">
-    <div class="task-view">
-      <section v-if="task">
-        <UFormGroup label="name" class="mb-4 w-full">
-          <UInput type="text" v-model="task.name"/>
-        </UFormGroup>
-        <UFormGroup label="description" class="mb-4 w-full">
-          <UTextarea v-model="task.description" />
-        </UFormGroup>
-        <UButton
-            icon="i-heroicons-trash"
-            color="red"
-            @click="deleteTask"
-        >Delete task</UButton>
-      </section>
-      <section v-else>
-        <p>Task not found</p>
-      </section>
+    <div v-if="task" class="task-view">
+      <UFormGroup label="name" class="mb-4 w-full">
+        <UInput type="text" v-model="task.name"/>
+      </UFormGroup>
+      <UFormGroup label="description" class="mb-4 w-full">
+        <UTextarea v-model="task.description" />
+      </UFormGroup>
+      <UButton
+          icon="i-heroicons-trash"
+          color="red"
+          @click="deleteTask"
+      >Delete task</UButton>
     </div>
+    <div v-else>
+      <p>Task not found</p>
+    </div>
+
   </div>
 </template>
